0d779bf3c103a9a9e79cc752f24908f5fb8398c3
wuminqi <wuminqi@163.com>
2011-05-08 15:10:44 +0800

    new file

---
+++ helloc9.js
@@ -0,0 +1,13 @@
+var c9_port = process.env.C9_PORT;
+var c9_host = '0.0.0.0';
+/* Can access from http://helloc9.wuminqi.cloud9ide.com */
+
+var http = require('http');
+
+http.createServer(function(req,res){
+  res.writeHead(200,{'Content-Type':'text/plain'});
+  res.end('Hello World\n');
+//}).listen(c9_port,c9_host);
+}).listen(12265,'127.0.0.1');
+
+console.log("Server runnint at http://helloc9.wuminqi.cloud9ide.com");
